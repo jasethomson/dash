666d4b3d767df249b3cd66346405f818afc50bf7
jasethomson <jasethomson31@gmail.com>
2020-11-20 07:27:30 -0800

    added gitignore, webpack config and init'd and installed npm node modules

---
+++ weback.config.js
@@ -0,0 +1,42 @@
+require('dotenv/config');
+const path = require('path');
+
+const clientPath = path.resolve(__dirname, 'client');
+const clientPath = path.resolve(__dirname, 'server/public');
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx'];
+  },
+  entry: './client',
+  output: {
+    path: publicPath
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              '@/plugin-transform-react-jsx'
+            ]
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: publicPath,
+    historyApiFallback: true,
+    host: '0.0.0.0',
+    port: process.env.DEV_SERVER_PORT,
+    proxy: {
+      '/api': `http://localhost:${process.env.PORT}`
+    },
+    stats: 'minimal',
+    watchContentBase: true
+  }
+};
